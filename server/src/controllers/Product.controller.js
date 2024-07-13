6f341ff5b876774df55fd1934154d0864ac87a38
Felipe Mendes <felipemendes0411@gmail.com>
2024-07-13 12:27:18 -0300

    create product controller

---
+++ server/src/controllers/Product.controller.js
@@ -0,0 +1,18 @@
+class ProductController {
+  static async createProduct(req, res) {
+  }
+
+  static async productUpdate(req, res) {
+  }
+
+  static async productDelete(req, res) {
+  }
+
+  static async listProduct(req, res) {
+  }
+
+  static async productById(req, res) {
+  }
+}
+
+module.exports = ProductController;
