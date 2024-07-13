1bb80bf50cf1f99bf3779edf282c0afd851f71bc
Felipe Mendes <felipemendes0411@gmail.com>
2024-07-13 12:27:03 -0300

    add product controller in routes

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,29 +1,30 @@
 const { Router } = require("express");
+const ProductController = require("../controllers/Product.controller");
 
 const productRouter = Router();
 const routeName = "/product"
 
 productRouter.post(
     routeName,
-    () => {}
+    ProductController.createProduct
 );
 
 productRouter.put(
     routeName,
-    () => {}
+    ProductController.productUpdate
 );
 productRouter.delete(
     routeName + "/:id",
-    () => {}
+    ProductController.productDelete
 );
 
 productRouter.get(
     routeName + "/findAll",
-    () => {}
+    ProductController.listProduct
 );
 productRouter.get(
     routeName + "/:id",
-    () => {}
+    ProductController.productById
 );
 
 
