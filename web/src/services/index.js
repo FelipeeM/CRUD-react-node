cee0f155e663a19fc21eb888d882521f8a4de157
Felipe Mendes <felipemendes0411@gmail.com>
2024-07-16 15:16:50 -0300

    add url local host api

--- web/src/services/index.js
+++ web/src/services/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const apiService = axios.create({
-    baseURL: process.env.URL_API,
+    baseURL: process.env.URL_API || "http://localhost:3333/api/v1",
     timeout: 10000,
 });
 
