0a0e0695e0879f333ad077a275b182d9dc06a5b3
Felipe Mendes <felipemendes0411@gmail.com>
2024-07-16 15:24:51 -0300

    fix base url

--- web/src/services/index.js
+++ web/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiService = axios.create({
-    baseURL: process.env.URL_API || "http://localhost:3333/api/v1",
+    baseURL: process.env.NODE_ENV === 'development'?  "http://localhost:3333/api/v1" : process.env.URL_API,
     timeout: 10000,
 });
 
