6db5ee5fa8b8bcbfaf0e214a4880a19e3b82a878
Felipe Mendes <felipemendes0411@gmail.com>
2024-07-16 15:16:15 -0300

    change port server

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
-const port = parseInt(process.env.PORT) || 3000;
+const port = parseInt(process.env.PORT) || 3333;
 
 app.use(cors())
 app.use(express.json())
