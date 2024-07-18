d69ecbfbbbcde9e17780e997ea850deea65a2149
Felipe Mendes <felipemendes0411@gmail.com>
2024-07-17 21:58:14 -0300

    fix ==

--- web/src/utils/masks.js
+++ web/src/utils/masks.js
@@ -4,7 +4,7 @@ const monetaryMask = (value, initialLabel = "") => {
     value = value.replace(/([0-9]{2})$/g, ",$1");
     if (value.length > 6)
         value = value.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    return `${initialLabel} ${value == "NaN" ? "" : value.replace(/(\d)(?=(\d{3})+\.)/g, "$1.")}`;
+    return `${initialLabel} ${value === "NaN" ? "" : value.replace(/(\d)(?=(\d{3})+\.)/g, "$1.")}`;
 };
 
 export { monetaryMask }
